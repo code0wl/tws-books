5d0479b5fab079279e35f245bf2eac7b40c5487c
Oscar Lodriguez <oscarl@backbase.com>
2016-08-16 05:44:05 +0200

    update

--- vagrant/catalog/database_setup.py
+++ vagrant/catalog/database_setup.py
@@ -1,3 +1,33 @@
 import sys
-from sqlalchemy import Column, Foreignkey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
+Base = declarative_base()
+
+
+class Shelter(Base):
+    __tablename__ = "shelter"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(80), nullable=False)
+    address = Column(String(250))
+    city = Column(String(500))
+    state = Column(String(500))
+    zipCode = Column(String(500))
+    website = Column(String(500), nullable=True)
+
+
+class Puppy(Base):
+    __tablename__ = "puppy"
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
+    dateOfBirth = Column(String(1000))
+    picture = Column(String(5000), nullable=True)
+    gender = Column(String(50), nullable=False)
+    weight = Column(Integer, nullable=False)
+    shelter_id = Column(Integer, ForeignKey("shelter.id"))
+    shelter = relationship(Shelter)
+
+
+engine = create_engine("sqlite:///shelter.db")
+Base.metadata.create_all(engine)
