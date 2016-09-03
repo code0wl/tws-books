c10eb2992fc260cff06b6ee7b546cb976251b49f
Oscar Lodriguez <oscarl@backbase.com>
2016-09-03 17:48:06 +0200

    add item

--- vagrant/catalog/project.py
+++ vagrant/catalog/project.py
@@ -2,9 +2,13 @@ from flask import Flask, render_template, request, redirect, url_for
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Library, Book
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = './media/uploads'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 
 app = Flask(__name__, static_folder='./node_modules')
-
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 engine = create_engine('sqlite:///library.db')
 Base.metadata.bind = engine
@@ -18,7 +22,6 @@ def notFound():
 
 
 @app.route('/')
-@app.route('/<string:library_name>')
 def renderHome():
     return render_template('index.html')
 
@@ -42,7 +45,6 @@ def newBook(library_name, library_id):
                         author=request.form['author'],
                         released=request.form['released'],
                         publisher=request.form['publisher'],
-                        library_id=library_id,
                         category=request.form['category'],
                         cover=request.form['cover'])
 
@@ -59,6 +61,17 @@ def renderLib(library_name):
     library = session.query(Library)
     items = session.query(Book)
     return render_template('books.html', items=items, library=library)
+
+
+@app.route('/<string:library_name>/books/<int:book_id>/remove', methods=['GET', 'POST'])
+def deleteBook(library_name, book_id):
+    entry = session.query(Book).filter_by(id=book_id).one()
+    if request.method == 'POST':
+        session.delete(entry)
+        session.commit()
+        return redirect(url_for('renderLib', library_name=library_name, book_id=book_id))
+    else:
+        return render_template('remove.html', item=entry, library_name=library_name)
 
 
 if __name__ == '__main__':
